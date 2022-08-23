6996810b0103acd01603ea32dbca5f0541f3804f
unknown <you@example.com>
2022-08-23 12:57:30 +0600

    last

--- js/calculate.js
+++ js/calculate.js
@@ -1,31 +1,17 @@
 
-document.getElementById('calc-btn').addEventListener('click', function () {
-    const playerpricefield = document.getElementById('player-price')
-    const newpricestring = playerpricefield.value
-    const newprice = parseFloat(newpricestring)
-
-
-    const totalpriceelement = document.getElementById('total-price')
-    const totalpricestring = totalpriceelement.innerText
-    const totalprice = parseFloat(totalpricestring)
-
-    const newpricetotal = totalprice + newprice
-    totalpriceelement.innerText = newpricetotal
-})
-
 document.getElementById('calc-total').addEventListener('click', function () {
+    let totalPriceText = document.getElementById("total-price").innerText;
     const managerpricefield = document.getElementById('manager-price')
-    const newmanagerstring = managerpricefield.value
-    const newmanager = parseFloat(newmanagerstring)
+    const newmanagerstring = managerpricefield.value;
+
 
     const coachpricefield = document.getElementById('coach-price')
     const newcoachstring = coachpricefield.value
-    const newcoach = parseFloat(newcoachstring)
 
-    const finalpriceelement = document.getElementById('final-price')
-    const finalpricestring = finalpriceelement.innerText
-    const finalprice = parseFloat(finalpricestring)
 
-    const newfinalprice = newcoach + newmanager + finalprice
-    finalpriceelement.innerText = newfinalprice
+    let finalpriceelement = document.getElementById('final-price');
+    let totalValue = parseInt(finalpriceelement)
+    const totalExpenses = parseInt(totalPriceText) + parseInt(newmanagerstring) + parseInt(newcoachstring);
+
+    finalpriceelement.innerText = totalExpenses;
 })
--- js/list.js
+++ js/list.js
@@ -1,11 +1,18 @@
-const playerarrray = []
+const playerarrray = [];
 
 function display(curtplayer) {
     console.log(curtplayer)
     const playerlist = document.getElementById('player-list')
     playerlist.innerHTML = '';
+
+    let playerCount = 0;
+
     for (let i = 0; i < curtplayer.length; i++) {
-        // console.log(curtplayer[i].playername)
+
+        if (i == 5) {
+            return alert("you can't choose more than 5 player");
+        }
+
         const name = curtplayer[i].playername
 
         const tr = document.createElement('tr')
@@ -13,17 +20,32 @@ function display(curtplayer) {
         <th scope="row">${i + 1}</th>
         <td>${name}</td>
         `;
+        playerCount++;
         playerlist.appendChild(tr)
+
     }
+    document.getElementById('calc-btn').addEventListener("click", function () {
+        const playerPriceField = document.getElementById('player-price')
+        let calPlayerValue = playerPriceField.value;
+        if (calPlayerValue > 1000 || calPlayerValue < 1000) {
+            return alert("You have to choose 1000 per players");
+        }
+        let playerExpenses = parseInt(calPlayerValue) * playerCount;
+        console.log(playerExpenses)
+        let totalPriceText = document.getElementById("total-price");
+        totalPriceText.innerText = playerExpenses;
+
+
+    })
 }
 function addtolist(element) {
-    const firstplayer = element.parentNode.children[0].innerText
-    console.log(firstplayer)
+    const firstPlayer = element.parentNode.children[0].innerText
+    console.log(firstPlayer)
 
-    const playerobject = {
-        playername: firstplayer
+    const playerObject = {
+        playername: firstPlayer
     }
-    playerarrray.push(playerobject)
+    playerarrray.push(playerObject)
     // console.log(playerarrray)
     // console.log(playerarrray.length)
 
